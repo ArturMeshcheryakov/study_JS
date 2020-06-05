45133d0da17eb2efa0c56139a14220eb80932b8d
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-06-05 08:16:01 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 120000;
+let income = '50000';
+let addExpenses = 'Коммуналка, Интернет, Проезд, Питание, Развлечения';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
+let budgetDay;
 
-alert('lesson01');
-console.log('lesson01');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+
+console.log(addExpenses);
+
+budgetDay = money / 30;
+
+console.log(budgetDay);
