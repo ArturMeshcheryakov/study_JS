a216c69d4e9ee2d419e26fa26793354970c59dc0
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-06-10 19:29:20 +0300

    lesson05difficult

--- lesson_difficult/difficult.js
+++ lesson_difficult/difficult.js
@@ -5,10 +5,12 @@
 let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
+
 let arr = [];
 
 for (let i = 0; i < 7; i++) {
   let number;
+
   do {
     number = prompt('Введите число');
 
@@ -22,25 +24,22 @@ for (let i = 0; i < 7; i++) {
 
 
 for (let i = 0; i < arr.length; i++) {
-
   let strFirst;
   strFirst = arr[i].split('');
 
   if (+strFirst[0] === 2 || +strFirst[0] === 4) {
     console.log(strFirst.join(''));
   } else {
-    console.log(strFirst.join('') + ' Такое число не подойдет, должно быть значений, которые начинаются с "2" или "4"');
+    console.log(strFirst.join('') + ' Такое число не подойдет, должно быть значение, которое начинается с "2" или "4"');
   }
 }
 
 //2
 let result;
-let test;
 
 for (let i = 1; i <= 100; i++) {
   for (let j = 2; j <= 100; j++) {
     if (i % j !== 0 || i === j) {
-      test = i;
       result = true;
     } else {
       result = false;
